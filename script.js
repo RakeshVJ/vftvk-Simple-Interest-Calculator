62ad36986bc7870e0bacd4cc48c2589c45adc8e1
RakeshVJ <rakesh19087@cse,ssn,edu,in>
2021-05-19 12:54:15 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+    var P = +document.getElementById("principal").value;
+    if(P<=0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    var p=Number(P);
+    var R = +document.getElementById("rate").value;
+    var rate=Number(R);
+    var Y = +document.getElementById("years").value;
+    var years=Number(Y);
+    var year = new Date().getFullYear()+parseInt(Y);
+    document.getElementById("result").innerHTML="If you deposit "+p+
+    ", at an interest rate of "+rate+
+    ", you will receive an amoount of "+parseFloat(p*years*rate/100)+
+    ", by end of the year "+year;
 }
-        
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
